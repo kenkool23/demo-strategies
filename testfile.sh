c78c81601b298ab8d8d855b2b7564523b42b59c1
kendevops4 <ken4@k-kloud.net>
2022-08-26 20:27:59 -0400

    updated many things

--- testfile.sh
+++ testfile.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -eo pipefail
 greeting() {
-local _GITHUB_USER="${1}"
-local _str='Hello, $_GITHUB_USER !'
-echo $_str
+# local _GITHUB_USER="${1}"
+# local _str='Hello, $_GITHUB_USER !'
+echo Hello, $1
 return $?
 }
-greeting "StrategicFS"
+greeting $1
 RET=$?
-if true || [ ${RET} -ne 0 ] ; then
+if [ ${RET} -ne 0 ] ; then
 echo 'ERR: Failed to give greeting' > /dev/stderr
 else
 echo 'DEBUG: Greeted the Github User' > /dev/stderr
