183ea97c6b54a7c203e0c5b75dbaf6b990d61b47
kendevops4 <ken4@k-kloud.net>
2022-08-27 06:22:55 -0400

    final update

--- testfile.sh
+++ testfile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail
-greeting() {
+greeting() {            #remove VAR from line 3
 # local _GITHUB_USER="${1}"
 # local _str='Hello, $_GITHUB_USER !'
 echo Hello, $1
@@ -8,11 +8,12 @@ return $?
 }
 greeting $1
 RET=$?
-if [ ${RET} -ne 0 ] ; then
+if [ ${RET} -ne 0 ] ; then     # Fixed Bug by removing true ||
 echo 'ERR: Failed to give greeting' > /dev/stderr
 else
 echo 'DEBUG: Greeted the Github User' > /dev/stderr
-fi
+fi                             #replace Done with fi
 
 #remove VAR from line 3
 #replace Done with fi
+#Commented line 4/5 
